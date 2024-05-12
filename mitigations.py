2dc3b853ac5871831e5a4c347b8a3d9562d51f20
Matt Adams <mrwadams@gmail.com>
2024-05-12 13:51:59 +0100

    Refactor threat model, attack tree, and mitigation generation functions into separate files

---
+++ mitigations.py
@@ -0,0 +1,78 @@
+from mistralai.client import MistralClient
+from openai import OpenAI
+from openai import AzureOpenAI
+
+# Function to create a prompt to generate mitigating controls
+def create_mitigations_prompt(threats):
+    prompt = f"""
+Act as a cyber security expert with more than 20 years experience of using the STRIDE threat modelling methodology. Your task is to provide potential mitigations for the threats identified in the threat model. It is very important that your responses are tailored to reflect the details of the threats.
+
+Your output should be in the form of a markdown table with the following columns:
+    - Column A: Threat Type
+    - Column B: Scenario
+    - Column C: Suggested Mitigation(s)
+
+Below is the list of identified threats:
+{threats}
+
+YOUR RESPONSE (do not wrap in a code block):
+"""
+    return prompt
+
+
+# Function to get mitigations from the GPT response.
+def get_mitigations(api_key, model_name, prompt):
+    client = OpenAI(api_key=api_key)
+
+    response = client.chat.completions.create(
+        model = model_name,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant that provides threat mitigation strategies in Markdown format."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Access the content directly as the response will be in text format
+    mitigations = response.choices[0].message.content
+
+    return mitigations
+
+
+# Function to get mitigations from the Azure OpenAI response.
+def get_mitigations_azure(api_key, model_name, prompt):
+    client = AzureOpenAI(
+        azure_endpoint = azure_api_endpoint,
+        api_key = azure_api_key,
+        api_version = azure_api_version,
+    )
+
+    response = client.chat.completions.create(
+        model = azure_deployment_name,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant that provides threat mitigation strategies in Markdown format."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Access the content directly as the response will be in text format
+    mitigations = response.choices[0].message.content
+
+    return mitigations
+
+
+# Function to get mitigations from the Mistral model's response.
+def get_mitigations_mistral(api_key, model_name, prompt):
+    client = MistralClient(api_key=mistral_api_key)
+
+    response = client.chat(
+        model = mistral_model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant that provides threat mitigation strategies in Markdown format."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Access the content directly as the response will be in text format
+    mitigations = response.choices[0].message.content
+
+    return mitigations
